88c28be89f3b1391e4f63e030f868102c4aaaaac
ivy <469202716@qq.com>
2016-12-06 15:02:33 +0800

    all

---
+++ TestCocoapodsLib/TestCocoapodsLib.h
@@ -0,0 +1,13 @@
+//
+//  TestCocoapodsLib.h
+//  TestCocoapodsLib
+//
+//  Created by ivy on 16/12/6.
+//  Copyright © 2016年 ivy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestCocoapodsLib : NSObject
+
+@end
---
+++ TestCocoapodsLib/TestCocoapodsLib.m
@@ -0,0 +1,13 @@
+//
+//  TestCocoapodsLib.m
+//  TestCocoapodsLib
+//
+//  Created by ivy on 16/12/6.
+//  Copyright © 2016年 ivy. All rights reserved.
+//
+
+#import "TestCocoapodsLib.h"
+
+@implementation TestCocoapodsLib
+
+@end
